6d36473d7cd7750acd8ac5286715f34548422bf7
Yan Zhou <yzhou@twilio.com>
2016-04-29 16:43:36 -0700

    Use answerOnBridge=True

--- hackpack/app.py
+++ hackpack/app.py
@@ -18,10 +18,10 @@ app.config.from_pyfile('local_settings.py')
 def voice():
     to = request.values.get('To', None)
 
-    realm_str = ""
-    found_realm = re.search("[.]([^.]+?)[.]twilio[.]com", to)
-    if found_realm:
-        realm_str = "in {0}".format(found_realm.group(1))
+#    realm_str = ""
+#    found_realm = re.search("[.]([^.]+?)[.]twilio[.]com", to)
+#    if found_realm:
+#        realm_str = "in {0}".format(found_realm.group(1))
     
     found_pstn = re.search("^sip:[+]?1?([0-9]{10})@", to)
     if found_pstn:
@@ -30,12 +30,12 @@ def voice():
     response = twiml.Response()
 
     if to.startswith("sip:"):
-        response.say("Welcome, you called SIP {0}".format(realm_str))
-        with response.dial() as d:
+#        response.say("Welcome, you called SIP {0}".format(realm_str))
+        with response.dial(answerOnBridge=True) as d:
             d.sip(to)
     else:
-        response.say("Welcome, you called PSTN {0}".format(realm_str))
-        with response.dial(callerId=app.config['TWILIO_CALLER_ID']) as d:
+#        response.say("Welcome, you called PSTN {0}".format(realm_str))
+        with response.dial(answerOnBridge=True, callerId=app.config['TWILIO_CALLER_ID']) as d:
             d.number(to)
 
     return str(response)
