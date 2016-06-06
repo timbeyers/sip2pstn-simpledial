56af8c4b41f81285972dbb6cdec5fd863fe481b8
Yan Zhou <yzhou@twilio.com>
2016-06-06 10:13:48 -0700

    Allow a record parameter to be passed in

--- hackpack/app.py
+++ hackpack/app.py
@@ -28,15 +28,16 @@ def voice():
         to = "+1{0}".format(found_pstn.group(1))
 
     answer_on_bridge = str2bool(request.values.get('answerOnBridge', "True"))
+    record_param = request.values.get('record', 'do-not-record')
 
     response = twiml.Response()
 
     if to.startswith("sip:"):
-        with response.dial(answerOnBridge=answer_on_bridge) as d:
+        with response.dial(answerOnBridge=answer_on_bridge, record=record_param) as d:
             d.sip(to)
     else:
         caller_id = request.values.get('callerId', app.config['TWILIO_CALLER_ID'])
-        with response.dial(answerOnBridge=answer_on_bridge, callerId=caller_id) as d:
+        with response.dial(answerOnBridge=answer_on_bridge, callerId=caller_id, record=record_param) as d:
             d.number(to)
 
     return str(response)
